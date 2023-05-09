9ad05f80a731f5691c770acc4bd30e5785aef2a3
Dhyey Sapara <dhyey@simformsolutions.com>
2023-05-09 18:37:01 +0530

    changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
+  has_many :comments
 
   after_create_commit -> { broadcast_prepend_to "posts", partial: "posts/post", locals: { post: self }, target: "posts" }
 end
